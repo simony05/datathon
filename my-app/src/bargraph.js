b8e224f03062d3f57af7ec3c8bf50bf0cf007c2d
simony05 <67559452+simony05@users.noreply.github.com>
2024-01-29 15:18:12 -0800

    edit text

--- my-app/src/bargraph.js
+++ my-app/src/bargraph.js
@@ -161,12 +161,12 @@ const MyBarGraph = () => {
       <div className="row">
         <div className='Col1'>
           <h1>Alcohol</h1>
-          <div className="item">Remember to take hydrometer readings throughout the process to estimate alcohol content. If necessary, adjust sugar levels or dilute the wine to achieve your desired alcohol percentage</div>
-          <div className="item">Initial Specific Gravity: Before fermentation begins, measure the specific gravity of the wine must using a hydrometer. Float the hydrometer in a sample of the wine must in a graduated cylinder or sample jar. Note the reading where the hydrometer's scale intersects the liquid level.</div>
-          <div className="item">Fermentation: Allow the wine to ferment. During fermentation, the yeast will convert sugars into alcohol and carbon dioxide. The specific gravity will decrease as the sugar is consumed.</div>
-          <div className="item">Fine Specific Gravity: After fermentation is complete, measure the final specific gravity using the same method as in step 1.</div>
-          <div className="item">Tips: 
-Adding sugar to a liquid will increase its specific gravity, while diluting the liquid with water will decrease the specific gravity. Specific gravity is a measure of the density of a substance compared to the density of water.</div>
+          <div className="item">Specific gravity is a measure of the density of a substance compared to the density of water.
+Initial Specific Gravity: Before fermentation begins, measure the specific gravity of the wine using a hydrometer. Float the hydrometer in a sample of the wine in a graduated cylinder or sample jar. 
+Allow the wine to ferment. During fermentation, the yeast will convert sugars into alcohol and carbon dioxide. The specific gravity will decrease as the sugar is consumed.
+Fine Specific Gravity: After fermentation is complete, measure the final specific gravity using the same method as above.
+Tips: Adding sugar to a liquid will increase its specific gravity, while diluting the liquid with water will decrease the specific gravity.
+</div>
         </div>
         <div className='Col1'>
           <h1>Sulphate</h1>
