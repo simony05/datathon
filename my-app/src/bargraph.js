254f7ca9b1bdb17b0c58b4ec7ebe0f3c84e615a7
simony05 <67559452+simony05@users.noreply.github.com>
2024-01-29 15:24:34 -0800

    text

--- my-app/src/bargraph.js
+++ my-app/src/bargraph.js
@@ -173,21 +173,16 @@ Tips: Adding sugar to a liquid will increase its specific gravity, while dilutin
           <div className="item">
             <Tex2SVG display="inline" latex="Alcohol=(((InitialSG - FinalSG) x 131.25) / 7.362" />
           </div>
-          <div className="item">In potassium sulfate (g/liter)</div>
-          <div className="item">These compounds release sulfur dioxide when added to wine, helping to protect it from spoilage, oxidation, and unwanted microbial activity.</div>
-          <div className="item">Based on the difference between the current sulfite level and the desired level, calculate the amount of potassium metabisulfite needed for adjustment. The calculation will depend on the volume of wine and the desired increase in SO2.</div>
-          <div className="item">Crush campden tablets or measure the appropriate amount of potassium metabisulfite powder. Dissolve it in a small amount of warm water to create a sulfite solution. Stir until fully dissolved.</div>
-          <div className="item">Slowly add the sulfite solution to the red wine, stirring gently to ensure even distribution. It's essential not to overshoot the desired sulfite level, so add the sulfite solution incrementally and retest if necessary.</div>
-          <div className="item">After making the sulfite adjustment, wait for some time to allow the sulfites to fully integrate into the wine. Retest the sulfur dioxide level using the testing kit to ensure that it meets the desired target.</div>
+          <div className="item">These compounds release sulfur dioxide when added to wine, helping to protect it from spoilage, oxidation, and unwanted microbial activity. Based on the difference between the current sulfite level and the desired level, calculate the amount of potassium metabisulfite needed for adjustment. The calculation will depend on the volume of wine and the desired increase in SO2. Measure the appropriate amount of powder and dissolve in warm water to create a sulphate solution before slowly adding it to the red wine. Ensure even distribution. Wait for some time to allow the sulfate to fully integrate into the wine before retesting to ensure that it meets the desired target.</div>
         </div>
         <div className='Col1'>
           <h1>Citric Acid</h1>
-          <div className="item">In winemaking, citric acid is sometimes used as an additive to adjust the acidity of the wine. The acidity of wine is an essential factor that influences its taste, balance, and stability.</div>
-          <div className="item">Citric acid can be added to the must (crushed grapes or juice) to increase the overall acidity of the wine. This is particularly relevant if the grapes used for winemaking are low in natural acidity. Adjusting acidity is crucial for achieving a balanced and harmonious flavor profile in the finished wine.</div>
-          <div className="item">Citric acid has antioxidant properties, which means it can help prevent oxidation in wine. While sulfur dioxide (SO2) is the primary antioxidant used in winemaking, citric acid can contribute to the overall antioxidant capacity of the wine.</div>
-          <div className="item">In small amounts, citric acid can enhance the fruitiness and brightness of the wine's flavors. It can be used to lift the overall flavor profile, especially in wines made from low-acid grapes.</div>
-          <div className="item">Measure the calculated amount of citric acid powder. Dissolve it in a small amount of water to create a citric acid solution. Stir until fully dissolved.</div>
-          <div className="item">(g/liter)</div>
+          <div className="item">In winemaking, citric acid is used as an additive to adjust the acidity of the wine, an essential factor that influences its taste, balance, and stability.
+Citric acid can be added to the must (crushed grapes or juice) to increase the overall acidity of the wine, particularly relevant if the grapes used for winemaking are low in natural acidity. Adjusting acidity is crucial for achieving a balanced and harmonious flavor profile in the finished wine.
+Citric acid has antioxidant properties, which means it can help prevent oxidation in wine. While sulfur dioxide (SO2) is the primary antioxidant used in winemaking, citric acid can contribute to the overall antioxidant capacity of the wine.
+In small amounts, citric acid can enhance the fruitiness and brightness of the wine's flavors. It can be used to lift the overall flavor profile, especially in wines made from low-acid grapes.
+Measure the calculated amount of citric acid powder. Dissolve it in a small amount of water to create a citric acid solution. Stir until fully dissolved.
+</div>
         </div>
         <div className='Col1'>
           <h1>Volatile Acidity</h1>
