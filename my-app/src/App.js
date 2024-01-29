d1b9e2731c85cf0ecfc4250401d4e53fdcbaf3a4
Kenneth Tan <kennethtan@ucsb.edu>
2024-01-28 21:17:38 -0800

    h

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import MyBarGraph from "./bargraph.js";
 import * as React from 'react';
-import MySlider from './slider.js';
 
 function App() {
   return (
     <div className="App">
-      <MySlider />
       <MyBarGraph />
     </div>
   );
--- my-app/src/bargraph.js
+++ my-app/src/bargraph.js
@@ -7,14 +7,17 @@ import Box from '@mui/material/Box';
 Chart.register(...registerables);
 
 const MyBarGraph = () => {
-  const [sliderValue, setSliderValue] = useState(50);
+  const [sliderValue, setSliderValue] = useState(10.0);
+  const [sliderValue1, setSliderValue1] = useState(10.0);
+  const [sliderValue2, setSliderValue2] = useState(10.0);
+  const [sliderValue3, setSliderValue3] = useState(10.0);
 
   const data = {
-    labels: ['Label 1', 'Label 2', 'Label 3'],
+    labels: ['Label 1', 'Label 2', 'Label 3', 'Label 4'],
     datasets: [
       {
         label: 'My Dataset',
-        data: [sliderValue, sliderValue * 2, sliderValue * 3],
+        data: [sliderValue, sliderValue1, sliderValue2, sliderValue3],
         backgroundColor: 'rgba(75, 192, 192, 0.2)',
         borderColor: 'rgba(75, 192, 192, 1)',
         borderWidth: 1,
@@ -33,6 +36,15 @@ const MyBarGraph = () => {
   const handleSliderChange = (event, newValue) => {
     setSliderValue(newValue);
   };
+  const handleSliderChange1 = (event, newValue) => {
+    setSliderValue1(newValue);
+  };
+  const handleSliderChange2 = (event, newValue) => {
+    setSliderValue2(newValue);
+  };
+  const handleSliderChange3 = (event, newValue) => {
+    setSliderValue3(newValue);
+  };
 
   return (
     <div>
@@ -47,6 +59,40 @@ const MyBarGraph = () => {
         max={20.0}
         step={0.1}/>
         <div>Value: {sliderValue}</div>
+
+        <Slider value={sliderValue1} onChange={handleSliderChange1} 
+        width={300}
+        defaultValue={10.0}
+        size="small"
+        aria-label="Small"
+        valueLabelDisplay="auto"
+        min={1.0}
+        max={20.0}
+        step={0.1}/>
+        <div>Value: {sliderValue1}</div>
+
+        <Slider value={sliderValue2} onChange={handleSliderChange2} 
+        width={300}
+        defaultValue={10.0}
+        size="small"
+        aria-label="Small"
+        valueLabelDisplay="auto"
+        min={1.0}
+        max={20.0}
+        step={0.1}/>
+        <div>Value: {sliderValue2}</div>
+
+        <Slider value={sliderValue3} onChange={handleSliderChange3} 
+        width={300}
+        defaultValue={10.0}
+        size="small"
+        aria-label="Small"
+        valueLabelDisplay="auto"
+        min={1.0}
+        max={20.0}
+        step={0.1}/>
+        <div>Value: {sliderValue3}</div>
+
       </Box>
       <Bar data={data} options={options} />
       
