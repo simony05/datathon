04183188ba6c4483de4226aaf037079d70e067d1
simony05 <67559452+simony05@users.noreply.github.com>
2024-01-28 20:36:13 -0800

    upadte

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,28 +8,14 @@ import Slider from '@mui/material/Slider';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <Box sx={{ width: 300 }}>
         <Slider
           size="small"
           defaultValue={4.6}
           aria-label="Small"
           valueLabelDisplay="auto"
-          min={4.6}
-          max={15.9}
+          min={1.0}
+          max={20.0}
           step={0.1}
         />
         <Slider defaultValue={50} aria-label="Default" valueLabelDisplay="auto" />
