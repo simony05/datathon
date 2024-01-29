800c1858c3e50ed28108fcd5943120510cf03e79
simony05 <67559452+simony05@users.noreply.github.com>
2024-01-28 20:47:19 -0800

    made a slider

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,24 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import BarChart from "./bargraph.js";
 import * as React from 'react';
-import Box from '@mui/material/Box';
-import Slider from '@mui/material/Slider';
+import MySlider from './slider.js';
 
 function App() {
   return (
     <div className="App">
-      <Box sx={{ width: 300 }}>
-        <Slider
-          size="small"
-          defaultValue={4.6}
-          aria-label="Small"
-          valueLabelDisplay="auto"
-          min={1.0}
-          max={20.0}
-          step={0.1}
-        />
-        <Slider defaultValue={50} aria-label="Default" valueLabelDisplay="auto" />
-      </Box>
+      <MySlider/>
       <BarChart />
     </div>
   );
--- my-app/src/slider.js
+++ my-app/src/slider.js
@@ -1,21 +1,32 @@
 import React, { useState } from 'react';
-import Range from 'react-range';
+import Box from '@mui/material/Box';
+import Slider from '@mui/material/Slider';
 
-const MyRangeSlider = () => {
-  const [values, setValues] = useState([20, 50]);
+const MySlider = () => {
+  const [sliderValue, setSliderValue] = useState(0);
+
+  const handleSliderChange = (event, newValue) => {
+    setSliderValue(newValue);
+  };
 
   return (
     <div>
-      <Range
-        values={values}
-        onChange={setValues}
-        min={0}
-max={100}
-        step={1}
-      />
-      <div>Values: {values.join(', ')}</div>
+        <Box sx={{ width: 300 }}>
+            <Slider
+            size="small"
+            value={sliderValue}
+            onChange={handleSliderChange}
+            defaultValue={10.0}
+            aria-label="Small"
+            valueLabelDisplay="auto"
+            min={1.0}
+            max={20.0}
+            step={0.1}
+            />
+        </Box>
+      <div>Value: {sliderValue}</div>
     </div>
   );
 };
 
-export default MyRangeSlider;
+export default MySlider;
